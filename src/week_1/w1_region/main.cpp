c5d12e6f4a6978078b5f7d9353adfe1f315f1edf
StasV <donsgenii@gmail.com>
2019-11-05 20:28:06 +0300

    Solved(w1_region). w1_region has been solved.

--- src/week_1/w1_region/main.cpp
+++ src/week_1/w1_region/main.cpp
@@ -2,3 +2,25 @@
 // Created by stas on 5.11.19.
 //
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <tuple>
+
+using namespace std;
+
+bool operator<(const Region& left, const Region& right){
+    return tie(left.std_name, left.parent_std_name, left.names, left.population) <
+           tie(right.std_name, right.parent_std_name, right.names, right.population);
+}
+
+int FindMaxRepetitionCount(const vector<Region>& regions){
+    map<Region, int> regions_map;
+    int max_ = 0;
+    for(const auto& item : regions){
+        ++regions_map[item];
+        max_ = max(max_, regions_map[item]);
+    }
+    return max_;
+}
