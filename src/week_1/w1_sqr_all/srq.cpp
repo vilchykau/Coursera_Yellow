7fd90c50bb3ccc9a5c5339b281fdc72d9ce8f53b
StasV <donsgenii@gmail.com>
2019-11-08 01:05:49 +0300

    Solved(w1_sqr_all). w1_sqr_all has been solved.

--- src/week_1/w1_sqr_all/srq.cpp
+++ src/week_1/w1_sqr_all/srq.cpp
@@ -1,4 +1,52 @@
 //
 // Created by stas on 8.11.19.
 //
+#include <vector>
+#include <iostream>
+#include <map>
+#include <utility>
 
+using namespace std;
+
+template <typename T>
+T Sqr(T value);
+
+template <typename T>
+vector<T> Sqr(const vector<T>& old_vector);
+
+template <typename T_KEY, typename T_VALUE>
+map<T_KEY, T_VALUE> Sqr(const map<T_KEY, T_VALUE>& old_map);
+
+template <typename T_FIRST, typename T_SECOND>
+pair<T_FIRST, T_SECOND> Sqr(const pair<T_FIRST, T_SECOND>& old_pair);
+
+
+
+template <typename T>
+T Sqr(T value){
+    return value * value;
+}
+
+template <typename T>
+vector<T> Sqr(const vector<T>& old_vector){
+    auto new_vector = old_vector;
+    for(auto& item : new_vector){
+        item = Sqr(item);
+    }
+    return new_vector;
+}
+
+template <typename T_KEY, typename T_VALUE>
+map<T_KEY, T_VALUE> Sqr(const map<T_KEY, T_VALUE>& old_map){
+    auto new_map = old_map;
+    for(auto& pair : new_map){
+        pair.second = Sqr(pair.second);
+    }
+    return new_map;
+}
+
+template <typename T_FIRST, typename T_SECOND>
+pair<T_FIRST, T_SECOND> Sqr(const pair<T_FIRST, T_SECOND>& old_pair){
+    return make_pair<T_FIRST, T_SECOND>(Sqr(old_pair.first),
+                                        Sqr(old_pair.second));
+}
--- src/week_1/w1_sqr_all/test.cpp
+++ src/week_1/w1_sqr_all/test.cpp
@@ -1,4 +1,22 @@
 //
 // Created by stas on 8.11.19.
 //
+#include "srq.cpp"
 
+int main(){
+    vector<int> v = {1, 2, 3};
+    cout << "vector:";
+    for (int x : Sqr(v)) {
+        cout << ' ' << x;
+    }
+    cout << endl;
+
+    map<int, pair<int, int>> map_of_pairs = {
+            {4, {2, 2}},
+            {7, {4, 3}}
+    };
+    cout << "map of pairs:" << endl;
+    for (const auto& x : Sqr(map_of_pairs)) {
+        cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
+    }
+}
