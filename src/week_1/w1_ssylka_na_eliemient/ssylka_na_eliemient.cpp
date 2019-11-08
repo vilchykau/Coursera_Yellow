f81a69c00ea4a68fca943028c8e1b0221e05a174
StasV <donsgenii@gmail.com>
2019-11-08 11:03:39 +0300

    Solved(w1_ssylka_na_element). Task has been solved.

--- src/week_1/w1_ssylka_na_eliemient/ssylka_na_eliemient.cpp
+++ src/week_1/w1_ssylka_na_eliemient/ssylka_na_eliemient.cpp
@@ -2,3 +2,14 @@
 // Created by stas on 8.11.19.
 //
 
+#include <map>
+#include <exception>
+using namespace std;
+
+template <typename T_KEY, typename T_VALUE>
+T_VALUE& GetRefStrict(map<T_KEY, T_VALUE>& m, T_KEY key){
+    if(!m.count(key)){
+        throw runtime_error("");
+    }
+    return m[key];
+}
--- src/week_1/w1_ssylka_na_eliemient/test.cpp
+++ src/week_1/w1_ssylka_na_eliemient/test.cpp
@@ -2,3 +2,12 @@
 // Created by stas on 8.11.19.
 //
 
+#include <iostream>
+#include "ssylka_na_eliemient.cpp"
+
+int main(){
+    map<int, string> m = {{0, "value"}};
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl; // выведет newvalue
+}
