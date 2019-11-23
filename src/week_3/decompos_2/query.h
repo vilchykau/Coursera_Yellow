7c54fb3a21cf1fc6f2d6770a0e8e6e7c1b757945
Stanislau Vilchykau <donsgenii@gmail.com>
2019-11-23 18:31:08 +0300

    soved the decompos_2 task

---
+++ src/week_3/decompos_2/query.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+enum class QueryType {
+    NewBus,
+    BusesForStop,
+    StopsForBus,
+    AllBuses
+};
+
+struct Query {
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
+};
+
+istream& operator >> (istream& is, Query& q);
